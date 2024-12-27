54ca971c1de31462a284f4cea55dc3fdce0265f8
Denish Acharya <83641102+denishacharya@users.noreply.github.com>
2024-12-27 18:18:51 +0545

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -2,7 +2,7 @@
 public class Patterns {
     public static void main(String[] args) {
         // Pattern 1
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 7; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
@@ -12,7 +12,17 @@ public class Patterns {
         System.out.println();
 
         // Pattern 2
-        for (int i = 5; i >= 1; i--) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        
+        // Pattern 3
+        for (int i = 7; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
@@ -21,14 +31,7 @@ public class Patterns {
 
         System.out.println();
 
-        // Pattern 3
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
+        
         System.out.println();
 
         // Pattern 4
